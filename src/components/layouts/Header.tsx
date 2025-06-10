72a778ae662118d5e6900810183ff69de4051e03
phuoc <connguaho@gmail.com>
2025-06-10 10:34:36 +0700

    refactor: enhance Header component with mobile menu and notification handling

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { SidebarTrigger } from "@/components/ui/sidebar";
-import { Menu, X, BellIcon  } from "lucide-react";
+import { BellIcon  } from "lucide-react";
 import { useNotifications } from "@/context/NotificationContext";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
 import NotificationTable from "@/components/notification/NotificationTable";
+import { Button } from "../ui/button";
 
 const Header: React.FC = () => {
   const { unreadCount, fetchNotifications } = useNotifications();
@@ -58,14 +59,14 @@ const Header: React.FC = () => {
         <div className="md:hidden flex items-center gap-2">
           <Sheet onOpenChange={(open) => { setIsMobileMenuOpen(open); handleOpenNotification(); }}>
             <SheetTrigger asChild>
-              <button className="relative text-white focus:outline-none hover:text-gray-300 transition-colors">
-                <BellIcon size={24} />
+              <Button className="relative text-white focus:outline-none hover:text-gray-300 transition-colors bg-[linear-gradient(to_right,#43978D,#264D59)] !border-0 ">
+                <BellIcon size={24} className=""/>
                 {unreadCount > 0 && (
                   <span className="absolute -top-1 -right-1 bg-red-500 text-white text-xs rounded-full h-4 w-4 flex items-center justify-center">
                     {unreadCount}
                   </span>
                 )}
-              </button>
+              </Button>
             </SheetTrigger>
             <SheetContent className="w-full sm:max-w-md">
               <SheetHeader>
@@ -77,13 +78,6 @@ const Header: React.FC = () => {
               <NotificationTable />
             </SheetContent>
           </Sheet>
-          <button
-            onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
-            className="text-white focus:outline-none hover:text-gray-300 transition-colors"
-            aria-label="Toggle menu"
-          >
-            {isMobileMenuOpen ? <X size={24} /> : <Menu size={24} />}
-          </button>
         </div>
     </header>
   );
